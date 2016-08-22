847ceecb5eb17848d594494089a4dacfe8c1d2fe
Grey <g.grey2411@gmail.com>
2016-08-21 22:45:34 -0400

    changed files from haml to erb

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -1,10 +1,12 @@
 class InstitutionsController < ApplicationController
+  #respond_to :json
   def index
     institutions = Institution.all
       #if phone = params[:phone]
       #  institutions = institutions.where(phone: phone)
       #end
-    render json: institutions, status: 200
+    render json: institutions, status: 200  
+    #respond_with(institutions, status: 200)
   end
   def new
     
