98b4bf776be864f680cf4c98dd83523df9d0264c
Greisy Guzman <g.grey2411@gmail.com>
2016-11-05 15:04:14 -0400

    Removed debugger

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,7 +9,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
    def create
-    debugger
     institution = Institution.find params[:user][:institution_id]
     params[:user][:institution_id] = institution.id
     role = Role.find params[:user][:role_id]
