a2ad5053118c98e8629241366db8ef8cb992fb95
Greisy Guzman <g.grey2411@gmail.com>
2016-11-12 15:15:21 -0400

    Created dropdown institution directive

--- app/assets/javascripts/scripts/directives/dropdown-institution.js
+++ app/assets/javascripts/scripts/directives/dropdown-institution.js
@@ -1,13 +1,29 @@
-angular.module("evalEasy")
+angular.module('evalEasy')
 	.directive('dropdownInstitution', function(){
 		return{
-			restrict: 'E',
-			templateUrl: 'views/directives/dropdown-institution.html',
+			restrict: "E",
+			templateUrl: "views/directives/dropdown-institution.html",
 			scope: {
 				institutions: "=",
-				search: "="
+				model: "="
 			},
-			link: function(scope, elem, attrs){
-			}	
+			link: function(scope, element, attrs){
+				element[0].querySelector("ul.collection").style.display ="none";
+				//console.log(element[0].querySelector("ul.collection").id);
+				//element.css("display", "none");
+				scope.setInstitution = function(institution){
+
+					scope.query =  institution.name;
+					scope.model = institution.id;
+					element[0].querySelector("ul.collection").style.display ="none";
+					debugger
+				};
+				scope.showCollection = function(){
+					element[0].querySelector("ul.collection").style.display ="block";
+				};
+				scope.hideCollection = function(){
+					element[0].querySelector("ul.collection").style.display ="none";
+				};
+			}
 		}
 	});
