944ac80983ad74215f7d092e9becc9a08b7a0cb7
Greisy Guzman <g.grey2411@gmail.com>
2017-05-13 21:35:00 -0400

    Set sendgrid as email delivery

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'app68328321@heroku.com',
-  :password => 'greisy941$',
-  :domain => 'eval-easy-lab.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,16 @@ Rails.application.configure do
   #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   #config.action_mailer.delivery_method = :smtp
   #config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  ActionMailer::Base.smtp_settings = {
+    :address  => 'smtp.sendgrid.net',
+    :port     => 587,
+    :domain   => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    #:user_name => ENV['SENDGRID_USERNAME'],
+    :password   => ENV['SENDGRID_PASSWORD'],
+    #w11okmnl2657
+    #:password   => ENV['SENDGRID_PASSWORD'],
+    :autentication => 'plain',
+    :enable_starttls_auto => true
+  }
 end
