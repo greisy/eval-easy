bd5c2e1b59b3c3f726b169c4c446036b8605bc2c
greisy <g.grey2411@gmail.com>
2017-08-22 11:03:28 -0400

    Starting again

--- db/schema.rb
+++ db/schema.rb
@@ -13,19 +13,22 @@
 
 ActiveRecord::Schema.define(version: 20170420020224) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "document_types", force: :cascade do |t|
-    t.string   "code",        limit: 255
-    t.text     "description", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "code"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "environment_users", force: :cascade do |t|
-    t.integer  "user_id",        limit: 4
-    t.integer  "role_id",        limit: 4
-    t.integer  "environment_id", limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.integer  "environment_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   add_index "environment_users", ["environment_id"], name: "index_environment_users_on_environment_id", using: :btree
@@ -33,57 +36,57 @@ ActiveRecord::Schema.define(version: 20170420020224) do
   add_index "environment_users", ["user_id"], name: "index_environment_users_on_user_id", using: :btree
 
   create_table "environments", force: :cascade do |t|
-    t.integer  "institution_id", limit: 4
-    t.integer  "user_id",        limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "institution_id"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   add_index "environments", ["institution_id"], name: "index_environments_on_institution_id", using: :btree
   add_index "environments", ["user_id"], name: "index_environments_on_user_id", using: :btree
 
   create_table "institutions", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "roles", force: :cascade do |t|
-    t.integer  "permission_level", limit: 4
-    t.string   "description",      limit: 255
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.integer  "permission_level"
+    t.string   "description"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "scale_types", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.string   "description", limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "scales", force: :cascade do |t|
-    t.string   "name",                  limit: 255
-    t.integer  "minimum_grade",         limit: 4
-    t.integer  "maximum_grade",         limit: 4
-    t.integer  "grade_to_pass_default", limit: 4
-    t.text     "description",           limit: 65535
-    t.string   "alphabetic_scale",      limit: 255
-    t.integer  "scale_type_id",         limit: 4
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.integer  "minimum_grade"
+    t.integer  "maximum_grade"
+    t.integer  "grade_to_pass_default"
+    t.text     "description"
+    t.string   "alphabetic_scale"
+    t.integer  "scale_type_id"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
   create_table "subjects", force: :cascade do |t|
-    t.string   "code",           limit: 255
-    t.string   "name",           limit: 255
-    t.text     "description",    limit: 65535
-    t.integer  "scale_id",       limit: 4
-    t.integer  "grade_to_pass",  limit: 4
-    t.boolean  "round_up",                     default: false
-    t.datetime "created_at",                                   null: false
-    t.datetime "updated_at",                                   null: false
-    t.integer  "environment_id", limit: 4
+    t.string   "code"
+    t.string   "name"
+    t.text     "description"
+    t.integer  "scale_id"
+    t.integer  "grade_to_pass"
+    t.boolean  "round_up",       default: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "environment_id"
     t.date     "start_date"
     t.date     "end_date"
   end
@@ -91,27 +94,27 @@ ActiveRecord::Schema.define(version: 20170420020224) do
   add_index "subjects", ["scale_id"], name: "index_subjects_on_scale_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "confirmation_token",     limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
-    t.string   "identity_card",          limit: 255
-    t.string   "name",                   limit: 255
-    t.string   "last_name",              limit: 255
-    t.string   "phone",                  limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.integer  "document_type_id",       limit: 4
+    t.string   "unconfirmed_email"
+    t.string   "identity_card"
+    t.string   "name"
+    t.string   "last_name"
+    t.string   "phone"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.integer  "document_type_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
